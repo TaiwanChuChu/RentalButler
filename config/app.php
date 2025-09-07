ccdaa0352728c38b93b2727ee4f231ec2a2a0756
AlexLu <alex.lu@dunqian.com>
2025-09-07 12:54:33 +0800

    將 shell 機敏資訊抽成 env、整理環境變數

--- config/app.php
+++ config/app.php
@@ -123,4 +123,8 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+     * -------------------------
+     */
+    'owner_line_user_id' => env('OWNER_LINE_USER_ID'),
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,63 +5,74 @@ use LINE\Clients\MessagingApi\Model\ReplyMessageRequest;
 use LINE\Laravel\Facades\LINEMessagingApi;
 
 Route::post('/receive-line-bot-notify', function (\Illuminate\Http\Request $request) {
-    \Log::debug('$request', [
-        '$request' => $request->all()
-    ]);
     $events = $request->events;
-    foreach ($events as $event) {
-        $type = $event['type'] ?? null;
-        \Log::debug('type', [
-            $type,
-        ]);
+    try {
+        foreach ($events as $event) {
+            $type = $event['type'] ?? null;
+            $userId = Arr::get($event, 'source.userId');
+            $ownerId = config('app.owner_line_user_id');
 
-        // 只針對訊息回覆
-        if ($type !== 'message') {
-            return response()->noContent();
-        }
+            if ($userId !== $ownerId) {
+                \Log::debug('不是開發者，不回應');
+                return response()->noContent(200);
+            }
 
-        /** @var array $messageObject */
-        $messageObject = $event['message'];
-        \Log::debug('messageObject', [
-            $messageObject,
-        ]);
-        $messageType = $messageObject['type'] ?? null;
-        $message = $messageObject['text'] ?? null;
-        $replyMessage = 'Love U';
+            $groupId = Arr::get($event, 'source.groupId');
+            $testingGroupId = config('line-group.testing');
+            if ($groupId !== $testingGroupId) {
+                \Log::debug('當前並非在測試群組，不回應');
+                return response()->noContent(200);
+            }
 
-        if ($messageType === 'sticker') {
-            $replyMessage = '別一直傳貼圖敷衍我好嗎？';
-        }
+            // 只針對訊息回覆
+            if ($type !== 'message') {
+                return response()->noContent(200);
+            }
 
-        if ($messageType === 'image') {
-            $replyMessage = '傳什麼鳥照？難看死了！';
-        }
+            /** @var array $messageObject */
+            $messageObject = $event['message'];
+            $messageType = $messageObject['type'] ?? null;
+            $message = $messageObject['text'] ?? null;
+            $replyMessage = 'Love U';
 
-        if ($message === '今天天氣如何') {
-            $replyMessage = '天曉得，不會自己去 Google 哦';
-        }
+            if ($messageType === 'sticker') {
+                $replyMessage = '別一直傳貼圖敷衍我好嗎？';
+            }
 
-        $replyToken = $event['replyToken'] ?? null;
-        if (!$replyToken) {
-            \Log::error('沒有解析到 replyToken，無法回傳訊息', [
-                'event' => $event,
-            ]);
-        }
-        $pushMessageRequest = ReplyMessageRequest::fromAssocArray([
-            'replyToken' => $replyToken,
-            'messages' => [
-                [
-                    'type' => 'text',
-                    'text' => $replyMessage,
+            if ($messageType === 'image') {
+                $replyMessage = '傳什麼鳥照？難看死了！';
+            }
+
+            if ($message === '今天天氣如何') {
+                $replyMessage = '天曉得，不會自己去 Google 哦';
+            }
+
+            $replyToken = $event['replyToken'] ?? null;
+            if (!$replyToken) {
+                \Log::error('沒有解析到 replyToken，無法回傳訊息', [
+                    'event' => $event,
+                ]);
+                return response()->noContent(200);
+            }
+            $pushMessageRequest = ReplyMessageRequest::fromAssocArray([
+                'replyToken' => $replyToken,
+                'messages' => [
+                    [
+                        'type' => 'text',
+                        'text' => $replyMessage,
+                    ],
                 ],
-            ],
+            ]);
+
+            LINEMessagingApi::replyMessage($pushMessageRequest);
+        }
+    } catch (\Throwable $th) {
+        \Log::error('發生錯誤', [
+            'request' => $request,
+            'trace' => $th->getTraceAsString(),
+            'error' => $th->getMessage(),
         ]);
-        $response = LINEMessagingApi::replyMessage($pushMessageRequest);
-        \Log::info('response: ', [$response]);
     }
 
-//    $response = LINEMessagingApi::replyMessageRequest($pushMessageRequests);
-//    \Log::info('response: ', [
-//        $response,
-//    ]);
+    return response()->noContent(200);
 });
